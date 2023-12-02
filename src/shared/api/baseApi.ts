86cb93f6982d2c7500ef70987ce9092c1004a142
DanyPlaya <danila.lusuy@gmail.com>
2023-12-03 02:11:11 +0300

    feat: add env baseUrl

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const baseApi = axios.create(
     {
-        baseURL: "https://wttonline.ru/api/",
+        baseURL: import.meta.env.VITE_APP_BASE_API_URL,
         headers: {
             Accept: "application/json",
             "Access-Control-Allow-Origin": "*"
