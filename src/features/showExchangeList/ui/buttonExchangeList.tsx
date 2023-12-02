ec94a249315552ed99a10a84c54b8509aa3a82f2
DanyPlaya <danila.lusuy@gmail.com>
2023-12-03 02:04:28 +0300

    feat: add hidden button

--- src/features/showExchangeList/ui/buttonExchangeList.tsx
+++ src/features/showExchangeList/ui/buttonExchangeList.tsx
@@ -1,25 +1,31 @@
 import { useCurrencyStore, useGetDirections } from "@/entities/currency";
 import { Button } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 export const ButtonShowExchangeList = () => {
   const { valueFrom, valuteTo } = useCurrencyStore();
 
-  const { refetch, data } = useGetDirections({
+  const { refetch } = useGetDirections({
     valute_from: valueFrom,
     valute_to: valuteTo,
   });
-
+  const [isVisibleButton, setIsVisibleButton] = useState(false);
   const isBothDirectionsSelected =
     valueFrom.trim() !== "" && valuteTo.trim() !== "";
 
+  const handleClick = () => {
+    refetch();
+    setIsVisibleButton((prev) => !prev);
+  };
   return (
     <>
       {isBothDirectionsSelected && (
         <Button
+          hidden={isVisibleButton}
           as={motion.button}
           whileTap={{ scale: 0.9 }}
-          onClick={() => (console.log(data, valueFrom, valuteTo), refetch())}
+          onClick={handleClick}
         >
           Далее
         </Button>
