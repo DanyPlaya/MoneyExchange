1ac539b70c5db9ef86222ca9e6d7b44861b4e196
DanyPlaya <danila.lusuy@gmail.com>
2023-12-02 15:13:24 +0300

    feat: created chakraTheme and add fonts

---
+++ src/app/chakraTheme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+    fonts: {
+        heading: `'Rubik Variable', sans- serif`,
+        body: `'Rubik Variable', sans- serif`,
+    },
+    styles: {
+        global: {
+            'html, body': {
+                color: '#323232',
+
+            },
+        }
+    }
+
+
+})
+
